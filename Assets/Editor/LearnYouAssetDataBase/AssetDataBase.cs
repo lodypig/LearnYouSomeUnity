8632d32fef28896bee503d47088c58fd319f9b6b
tangt <lodypig@126.com>
2016-05-21 21:33:21 +0800

    Learn AssetDataBase.create() & AssetDataBase.copy()

---
+++ Assets/Editor/LearnYouAssetDataBase/AssetDataBase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AssetDataBase  {
+
+    [MenuItem("AssetDataBase/创建Asset")]
+    public static void CreateAsset() {
+        Material material = AssetDatabase.LoadAssetAtPath<Material>("Assets/Material/LearnYouAssetDataBase.mat");
+        Material newMaterial = new Material(material);
+        AssetDatabase.CreateAsset(newMaterial, "Assets/I'm create from LearnYouAssetDataBase.mat");
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("AssetDataBase/复制Materal")]
+    public static void CopyMateral() {
+        Material m = AssetDatabase.LoadAssetAtPath<Material>("Assets/Material/LearnYouAssetDataBase.mat");
+        AssetDatabase.CopyAsset("Assets/Material/LearnYouAssetDataBase.mat", "Assets/I'm a copy of LearnYouAssetDataBase.mat");
+        AssetDatabase.Refresh();
+    }
+
+
+
+
+    
+}
---
+++ Assets/Editor/LearnYouAssetbundle/BuildAssetBundle.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+
+public class TestAB  {
+
+
+    public static void BuildForEachFolder(string path, string outPath, params string[] searchPatterns) {
+        string[] dirs = Directory.GetDirectories(Application.dataPath + path);
+        List<string> assetNames = new List<string>();
+        for (int i = 0; i < dirs.Length; ++i) {
+            for (int j = 0; j < searchPatterns.Length; ++j) { 
+                string[] files = Directory.GetFiles(dirs[i], searchPatterns[j]);
+                for (int k = 0; k < files.Length; ++i) {
+                    assetNames.Add(files[k]);
+                }
+            }
+        }
+        uint crc;
+        //BuildPipeline.BuildAssetBundle(null, assetNames.ToArray(), Application.streamingAssetsPath + "/" + outPath, out crc);
+    }
+
+
+    [MenuItem("AssetBundle/打包模型")]
+    public static void BuildCube() {
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[2];
+        string[] assetNames = new string[1];
+
+        assetNames[0] = "Assets/Prefab/Model/Cube.prefab";
+        buildMap[0].assetBundleName = "cube";
+        buildMap[0].assetNames = assetNames;
+
+        string[] assetNames2 = new string[1];
+        assetNames2[0] = "Assets/Prefab/Model/Capsule.prefab";
+        buildMap[1].assetBundleName = "capsule";
+        buildMap[1].assetNames = assetNames2;
+
+        BuildPipeline.BuildAssetBundles("Assets/AB/Model", buildMap);
+    }
+    [MenuItem("AssetBundle/打包UI")]
+    public static void BuildUI() {
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[3];
+        string[] files = Directory.GetDirectories(Application.dataPath + "/Prefab", "*.prefab");
+
+
+        BuildPipeline.BuildAssetBundles("Assets/AB/UI", buildMap);
+    }
+}
