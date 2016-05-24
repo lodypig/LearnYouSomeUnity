1f450db43e53a7184d923257f197662b7d6d0513
tangt <lodypig@126.com>
2016-05-24 20:02:45 +0800

    Learn AssetBundle.LoadFromFile & reorganize folders

---
+++ Assets/LearnYouAssetDataBase/Editor/AssetDataBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AssetDataBase  {
+
+    [MenuItem("AssetDataBase/创建Asset")]
+    public static void CreateAsset() {
+        Material material = AssetDatabase.LoadAssetAtPath<Material>("Assets/LearnYouAssetDataBase/Material/LearnYouAssetDataBase.mat");
+        Material newMaterial = new Material(material);
+        AssetDatabase.CreateAsset(newMaterial, "Assets/I'm create from LearnYouAssetDataBase.mat");
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("AssetDataBase/复制Materal")]
+    public static void CopyMateral() {
+        AssetDatabase.CopyAsset("Assets/LearnYouAssetDataBase/Material/LearnYouAssetDataBase.mat", "Assets/I'm a copy of LearnYouAssetDataBase.mat");
+        AssetDatabase.Refresh();
+    }
+
+
+
+
+    
+}
---
+++ Assets/LearnYouLoadAAssetbundle/LearnYouLoadAssetBundle.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LearnYouLoadAssetBundle : MonoBehaviour {
+
+
+    public static readonly string STREAMING_ASSET_PATH =
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR
+    Application.streamingAssetsPath;
+#elif UNITY_IPHONE  
+        Application.dataPath + "/Raw";  
+#elif UNITY_ANDROID
+    Application.dataPath + "!assets";  
+#else  
+        string.Empty;  
+#endif
+
+    private AssetBundle LoadFromFile(string path) {
+        return AssetBundle.LoadFromFile(STREAMING_ASSET_PATH + "/sprite1234.ab");
+    }
+
+    private void CreateAsset(AssetBundle ab, string assetName) {
+        GameObject asset = ab.LoadAsset(assetName) as GameObject;
+        if (asset != null) {
+            GameObject go= GameObject.Instantiate(asset);
+        }
+        ab.Unload(false);
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        AssetBundle ab = LoadFromFile("UI/Compress/sprite1234.ab");
+        CreateAsset(ab, "sprite1234.prefab");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
